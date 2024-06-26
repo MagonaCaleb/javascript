b95638f83bbb1fc4b0d57c4e2ebb86c764ec00ea
Caleb Magona <calebmagona@gmail.com>
2024-06-26 09:40:22 +0300

    grade checker

--- grade.js
+++ grade.js
@@ -1,34 +1,9 @@
-// ``` JavaScript Assignment: Student Grade Management using Object
-// Objective:
-// Create a JavaScript program that utilizes an object to manage student grades across multiple subjects. Implement functionalities to add grades for students, calculate the average grade, find a specific student's grades, list all students and their grades, and determine the overall best student based on total grades.
-
-// Instructions:
-// Data Setup:
-
-// Use an object where keys are student names (string) and values are arrays of integers representing grades for subjects (e.g., Math, Science, English, SST).
-// Tasks:
-
-// Implement methods to:
-// Add grades for a student: addGrade(studentName, grades).
-// Calculate and display the average grade for a student: calculateAverageGrade(studentName).
-// Find and display a specific student's grades: findStudentGrades(studentName).
-// List all students and their grades: listAllStudents().
-// Find and display the overall best student based on total grades: findOverallBestStudent().
-// Output:
-
-// Print the results of each method call clearly labeled (e.g., "Average Grade for John: 85").
-// Requirements:
-
-// Use an object to store student names and their corresponding grades.
-// Ensure proper error handling, such as checking for non-existent students when retrieving grades.
-// Provide a clear and structured code format with appropriate comments.
-
 
 
 let studentGrades = {
 
 }
-function addGrade(studentName, grades){
+let addGrade=(studentName, grades)=>{
     try {
         studentGrades[studentName] = grades;
       } catch (error) {
@@ -40,7 +15,7 @@ addGrade("Elisha Bantana", [82, 72, 51, 95])
 addGrade("Masaba Rahman", [20, 30, 60, 55])
 
 
-function calculateAverageGrade(studentName){
+let calculateAverageGrade=(studentName)=>{
     try {
         let sum = 0;
         for (let i = 0; i < studentGrades[studentName].length; i++) {
@@ -55,7 +30,7 @@ function calculateAverageGrade(studentName){
 
 calculateAverageGrade("Masaba Rahman")
 
-function findStudentGrades(studentName){
+let findStudentGrades = (studentName) => { 
     try {
         console.log(`Grades for ${studentName}: ${studentGrades[studentName]}`);
       } catch (error) {
@@ -65,7 +40,8 @@ function findStudentGrades(studentName){
 
 findStudentGrades("Masaba Rahman")
 
-function listAllStudents(){
+//arrow functions
+let listAllStudents = () =>{
     try {
         for (const student in studentGrades) {
             console.log(`${student}: ${studentGrades[student]}`);
@@ -77,7 +53,7 @@ function listAllStudents(){
 
 listAllStudents()
 
-function findOverallBestStudent(){
+let findOverallBestStudent=()=>{
     try {
         let bestStudent = "";
         let bestGrade = 0;
@@ -99,3 +75,6 @@ function findOverallBestStudent(){
 
 
 findOverallBestStudent()
+
+//arrow functions
+
